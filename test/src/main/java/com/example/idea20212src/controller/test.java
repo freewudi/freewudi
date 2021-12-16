26e64c7ac751628a097c1c499895f616f70f276f
freewudi <63575956+freewudi@users.noreply.github.com>
2021-12-16 13:35:07 +0800

    第三次测试

---
+++ test/src/main/java/com/example/idea20212src/controller/test.java
@@ -0,0 +1,11 @@
+package com.example.idea20212src.controller;
+
+import com.example.idea20212src.entity.Test;
+
+public class test {
+    public void test(){
+        Test t = new Test();
+        t.name="123";
+        System.out.println(t.name);
+    }
+}
