b9307ad909014cf7fbec147bf147258855e1ce6d
freewudi <63575956+freewudi@users.noreply.github.com>
2021-12-22 09:43:58 +0800

    静态方法改变作用域外的例子

---
+++ java/src/PassObject.java
@@ -0,0 +1,16 @@
+class Letter {
+    char c;
+    static void f(Letter y) {
+        y.c = 'z';
+    }
+}
+public class PassObject {
+
+    public static void main(String[] args) {
+        Letter x = new Letter();
+        x.c = 'a';
+        System.out.println("1: x.c: " + x.c);
+        Letter.f(x);
+        System.out.println("2: x.c: " + x.c);
+    }
+} 
