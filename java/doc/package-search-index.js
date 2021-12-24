b23c2c6d880ff1eed5dd8460e748bf253bc2c11b
freewudi <63575956+freewudi@users.noreply.github.com>
2021-12-24 10:57:12 +0800

    doc

---
+++ java/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"charact06","h":"","u":""},{"l":"charact3","h":"","u":""},{"l":"charact4","h":"","u":""},{"l":"charact5","h":"","u":""},{"l":"tools","h":"","u":""},{"l":"所有程序包","h":"","u":"allpackages-index.html"}]
